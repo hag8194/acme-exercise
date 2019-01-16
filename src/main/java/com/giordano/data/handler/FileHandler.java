90193c1c34ad85fca9d122cf66b9f233d08637cf
Hugo Giordano <hag8194@gmail.com>
2019-01-16 15:24:19 -0500

    code refactor

--- src/main/java/com/giordano/data/handler/FileHandler.java
+++ src/main/java/com/giordano/data/handler/FileHandler.java
@@ -19,23 +19,41 @@ public class FileHandler {
         classLoader = getClass().getClassLoader();
     }
 
+    private Stream<String> getStreamFromFile(Path path) {
+        Stream<String> stream = null;
+        try {
+            stream = Files.lines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stream;
+    }
+
+    private Path getPath(URI uri, Map<String, String> env, String[] array) throws IOException {
+        Path path;
+        if (array.length == 2) {
+            if (Objects.nonNull(fs))
+                fs.close();
+            fs = FileSystems.newFileSystem(URI.create(array[0]), env);
+            path = fs.getPath(array[1]);
+        } else {
+            path = Paths.get(uri);
+        }
+        return path;
+    }
+
+    private Stream<String> getStringStream(URI uri) throws IOException {
+        final Map<String, String> env = new HashMap<>();
+        final String[] array = uri.toString().split("!");
+        final Path path = getPath(uri, env, array);
+        return getStreamFromFile(path);
+    }
+
     public Stream<String> importWorkReportFile() {
         Stream<String> stream = null;
         try {
             URI uri = Objects.requireNonNull(classLoader.getResource(FileUtils.WORK_REPORT_FILE_PATH)).toURI();
-            final Map<String, String> env = new HashMap<>();
-            final String[] array = uri.toString().split("!");
-
-            final Path path;
-            if(array.length == 2) {
-                if(Objects.nonNull(fs))
-                    fs.close();
-                fs = FileSystems.newFileSystem(URI.create(array[0]), env);
-                path = fs.getPath(array[1]);
-            } else {
-                path = Paths.get(uri);
-            }
-            stream = getStreamFromFile(path);
+            stream = getStringStream(uri);
         } catch (URISyntaxException | IOException e) {
             e.printStackTrace();
         }
@@ -46,32 +64,11 @@ public class FileHandler {
         Stream<String> stream = null;
         try {
             URI uri = Objects.requireNonNull(classLoader.getResource(FileUtils.PAY_TABLE_FILE_PATH)).toURI();
-            final Map<String, String> env = new HashMap<>();
-            final String[] array = uri.toString().split("!");
-
-            final Path path;
-            if(array.length == 2) {
-                if(Objects.nonNull(fs))
-                    fs.close();
-                fs = FileSystems.newFileSystem(URI.create(array[0]), env);
-                path = fs.getPath(array[1]);
-            } else {
-                path = Paths.get(uri);
-            }
-            stream = getStreamFromFile(path);
+            stream = getStringStream(uri);
         } catch (URISyntaxException | IOException e) {
             e.printStackTrace();
         }
         return stream;
     }
 
-    private Stream<String> getStreamFromFile(Path path) {
-        Stream<String> stream = null;
-        try {
-            stream = Files.lines(path);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return stream;
-    }
 }
