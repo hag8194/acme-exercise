064972ea4881d2d4660f569588c664429e744abe
Hugo Giordano <hag8194@gmail.com>
2019-01-16 09:36:09 -0500

    added maven plugins to package jar

--- src/main/java/com/giordano/data/handler/FileHandler.java
+++ src/main/java/com/giordano/data/handler/FileHandler.java
@@ -3,20 +3,66 @@ package com.giordano.data.handler;
 import com.giordano.data.utils.FileUtils;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class FileHandler {
+    private ClassLoader classLoader;
+    private FileSystem fs = null;
+
+    public FileHandler() {
+        classLoader = getClass().getClassLoader();
+    }
+
     public Stream<String> importWorkReportFile() {
-        Path path = Paths.get(FileUtils.WORK_REPORT_FILE_PATH);
-        return getStreamFromFile(path);
+        Stream<String> stream = null;
+        try {
+            URI uri = Objects.requireNonNull(classLoader.getResource(FileUtils.WORK_REPORT_FILE_PATH)).toURI();
+            final Map<String, String> env = new HashMap<>();
+            final String[] array = uri.toString().split("!");
+
+            final Path path;
+            if(array.length == 2) {
+                if(Objects.nonNull(fs))
+                    fs.close();
+                fs = FileSystems.newFileSystem(URI.create(array[0]), env);
+                path = fs.getPath(array[1]);
+            } else {
+                path = Paths.get(uri);
+            }
+            stream = getStreamFromFile(path);
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+        return stream;
     }
 
     public Stream<String> importPayTableFile() {
-        Path path = Paths.get(FileUtils.PAY_TABLE_FILE_PATH);
-        return getStreamFromFile(path);
+        Stream<String> stream = null;
+        try {
+            URI uri = Objects.requireNonNull(classLoader.getResource(FileUtils.PAY_TABLE_FILE_PATH)).toURI();
+            final Map<String, String> env = new HashMap<>();
+            final String[] array = uri.toString().split("!");
+
+            final Path path;
+            if(array.length == 2) {
+                if(Objects.nonNull(fs))
+                    fs.close();
+                fs = FileSystems.newFileSystem(URI.create(array[0]), env);
+                path = fs.getPath(array[1]);
+            } else {
+                path = Paths.get(uri);
+            }
+            stream = getStreamFromFile(path);
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+        return stream;
     }
 
     private Stream<String> getStreamFromFile(Path path) {
--- src/main/java/com/giordano/data/utils/FileUtils.java
+++ src/main/java/com/giordano/data/utils/FileUtils.java
@@ -1,7 +1,7 @@
 package com.giordano.data.utils;
 
 public class FileUtils {
-    private static final String BASE_DIR = "src/main/resources/";
+    private static final String BASE_DIR = "";
     public static final String WORK_REPORT_FILE_PATH = BASE_DIR + "workreport.txt";
     public static final String PAY_TABLE_FILE_PATH = BASE_DIR + "paytable.txt";
 
