a4bc382e515211d0f6aca3096c61dfd8d9af0734
Hugo Giordano <hag8194@gmail.com>
2019-01-23 21:24:02 -0500

    updated MockPresentationData

--- src/test/java/com/giordano/presentation/MockPresentationData.java
+++ src/test/java/com/giordano/presentation/MockPresentationData.java
@@ -6,5 +6,5 @@ final class MockPresentationData {
         .append("The amount to pay ASTRID is: 85.0 USD\n")
         .append("The amount to pay PEDRO is: 215.0 USD\n")
         .append("The amount to pay JESUS is: 85.0 USD\n")
-        .append("The amount to pay CESAR is: 85.0 USD\n\r\n").toString();
+        .append("The amount to pay CESAR is: 85.0 USD\n\n").toString();
 }
